568efd6f89f1dc17bdb922101a70c161df64838e
Zhenya Telyukov <telyukov@gmail.com>
2019-02-11 17:01:36 +0300

    v.0.3 canvas

--- bounce/game_gui.py
+++ bounce/game_gui.py
@@ -9,5 +9,29 @@ from tkinter import *
 
 class GameGUI():
     def __init__(self, master, game):
-        pass
-        
+        self.master = master
+
+        # Default attribures
+        master.title('Bouncing a ball')
+        master.geometry(self.center(master))
+        master.attributes("-topmost", True)         # Place master over all other windows
+        master.config(background = 'lightyellow')
+        master.resizable(False, False)
+
+        # Widgets
+        self.canvas = Canvas(master, width=500, height=400)      # Game area
+        self.canvas.pack()
+        self.master.update()         # Initializes itself for the game animation
+    
+            # Locates GUI window in the center of a screen
+    def center(self, master):
+        masterWidth  = 800
+        masterHeight = 600
+
+        screenWidth  = master.winfo_screenwidth()
+        screenHeight = master.winfo_screenheight()
+
+        masterX = (screenWidth  - masterWidth)  / 2
+        masterY = (screenHeight - masterHeight) / 2
+
+        return('%dx%d+%d+%d' % (masterWidth, masterHeight, masterX, masterY))
